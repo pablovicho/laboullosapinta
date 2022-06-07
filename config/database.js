42c56d86845b085321084ff0042486ff5bf854b0
pablo <pablovvb@gmail.com>
2022-06-06 22:32:29 -0500

    changed database to postgres

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 4321),
-      database: env('DATABASE_NAME', 'strapi'),
+      database: env('DATABASE_NAME', 'postgres'),
       user: env('DATABASE_USERNAME', ''),
       password: env('DATABASE_PASSWORD', ''),
       ssl: env('DATABASE_URL') ? true : false
