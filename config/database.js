f1161dba812e5edb849c9758af51d0b41400719a
pablo <pablovvb@gmail.com>
2022-06-13 05:30:43 -0500

    initial commit

---
+++ config/database.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 4321),
+      database: env('DATABASE_NAME', 'postgres'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', '0000'),
+      // schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      // ssl: {
+      //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      // },
+    },
+    debug: false,
+  },
+});
