5a6269a94891e884583c507287c644e8ebf9aedd
pablovicho <pablovvb@gmail.com>
2022-11-07 13:22:04 -0600

    server config with only url

--- config/server.js
+++ config/server.js
@@ -1,8 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: 'https://laboullosapinta.up.railway.app'
+  url: 'https://laboullosapinta.up.railway.app',
 });
