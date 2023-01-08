d87d329ad3f352a1e02cab61bf6a93655687a238
pablovicho <pablovvb@gmail.com>
2023-01-07 21:27:54 -0600

    upgrade strapi instance and add SEO

--- config/server.js
+++ config/server.js
@@ -2,5 +2,7 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
-  url: "https://laboullosapinta.herokuapp.com/admin",
+  url: env("MY_HEROKU_URL"),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
 });
